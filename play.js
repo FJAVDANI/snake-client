4308244123de751d176e1cfbbdbf44b3d2bf411d
FJAVDANI <115185399+FJAVDANI@users.noreply.github.com>
2023-07-07 03:24:29 +0530

    Update play.js

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:127.0.0.1,
-    port:80 ,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-   
-  conn.on("connect", () => {
-  console.log("connect succesful");
-});
-  
-module
-  return conn;
-};
-
-module.export = {
-  connect
-};
+const {connect} = require("./play");
+console.log("Connecting ...");
+connect;
 
