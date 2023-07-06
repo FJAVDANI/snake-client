12557008b4edc530ec2a61eff1b0489030accf88
FJAVDANI <115185399+FJAVDANI@users.noreply.github.com>
2023-07-07 03:07:18 +0530

    Create client.js

---
+++ client.js
@@ -0,0 +1,14 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: // IP address here,
+    port: // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
