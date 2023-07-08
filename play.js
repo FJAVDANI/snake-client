fbac498c468d3f7fe6f0ef2b32e958bf317a8515
FJAVDANI <115185399+FJAVDANI@users.noreply.github.com>
2023-07-08 21:58:12 +0530

    Update play.js

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
-const {connect} = require("./play");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log("Connecting ...");
-connect;
+connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-};
-
-const handleUserInput = function () {
-  // your code here
-};
+setupInput();
 
