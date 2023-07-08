c12c1e6ddf783589b3a31e7430bcc14cf22e4af2
FJAVDANI <115185399+FJAVDANI@users.noreply.github.com>
2023-07-08 22:18:57 +0530

    Update constand.js

--- constand.js
+++ constand.js
@@ -7,3 +7,32 @@ module.exports = {
 };
 
 
+
+const connect = function () {
+  const conn = net.createConnection({
+    Name:"joh"
+    Move :up
+    host:127.0.0.1,
+    port:80 ,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+   
+  conn.on("connect", () => {
+  console.log("Successfully connected to game server");
+});
+  
+module
+  return conn;
+};
+
+module.exports = {
+  IP,
+  PORT,
+  connect
+}
+
+
+
+
