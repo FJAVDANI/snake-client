45df2aea750dd11acd100cfda12eb2397e9190bc
FJAVDANI <115185399+FJAVDANI@users.noreply.github.com>
2023-07-08 21:43:06 +0530

    Update play.js

--- play.js
+++ play.js
@@ -9,6 +9,12 @@ const setupInput = function () {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
+  
   return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
 };
 
