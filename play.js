a8cae0981d7806393709357307fcbb8512bdb62d
FJAVDANI <115185399+FJAVDANI@users.noreply.github.com>
2023-07-08 21:35:54 +0530

    Update play.js

--- play.js
+++ play.js
@@ -2,3 +2,13 @@ const {connect} = require("./play");
 console.log("Connecting ...");
 connect;
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
